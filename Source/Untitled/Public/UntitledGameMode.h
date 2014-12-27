55ecf8725b07e8ffc742425ec3b84a094cdc89b4
Zach Nickell <zach.x.nickell@gmail.com>
2014-12-27 15:07:27 -0600

    first

---
+++ Source/Untitled/Public/UntitledGameMode.h
@@ -0,0 +1,14 @@
+// Copyright 1998-2014 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "UntitledGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUntitledGameMode : public AGameMode
+{
+	GENERATED_UCLASS_BODY()
+
+};
+
+
+
